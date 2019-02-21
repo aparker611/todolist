e86e290a49de38e7037cced30ee712ef9c685dd4
aparker611 <adam3692@hotmail.co.uk>
2019-02-21 08:17:42 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var $submit = $('.submit');
+  var $input  = $('.addit');
+  var $itemContainer = $('.item');
+
+  $itemContainer.hide();
+
+  function inputLength(input) {
+    return $input.value.length();
+  }
+
+  function returnInputLength(input) {
+    return $input.value.length();
+  }
+
+  function addItem(input){
+  }
+});
