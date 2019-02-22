adb2cbd4e276f3fafb1ac47932309d80d4c3a592
aparker611 <adam3692@hotmail.co.uk>
2019-02-22 21:17:28 +0000

    fixed issue with input being allowed to be blank

--- js/main.js
+++ js/main.js
@@ -8,17 +8,20 @@ $(document).ready(function() {
 <button class="remove"></button>
 
 */
+// when you click the submit button it checks that the length is not longer than 28
+// it then calls the addListItem function if it is okay.
 $submit.click(function( e ) {
   e.preventDefault();
-  if ($input.value.length > 27) {
+  if ($input.value.length > 27 || $input.value.length === 0) {
     $input.value = "";
-    alert("Please enter an amount under 28 characters");
+    alert("Please enter an amount under 28 characters and not blank");
     return false;
   } else {
     addListItem();
     return false;
   }
 });
+// after making sure the
   function addListItem() {
     var $li = "<li class='listItem'>" +
               "<p class='item-name'>"+$input.value+"</p>" +
