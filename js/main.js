095642801af5132f65e3386779437fa5fa86cd7d
aparker611 <adam3692@hotmail.co.uk>
2019-02-22 21:11:33 +0000

    finished? added checks to make sure the input is less than 28 characters but atleast more than one, added styles for things that are done when clicked and added remove functionality

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
 $(document).ready(function() {
   var $submit = $('.submit');
-  var $input  = $('.addit');
-  var $itemContainer = $('.item');
+  var $input  = $('.additem')[0];
+  var $ul     = $('.item-list');
+  var $done   = false;
+/*
+<p class="item-name"></p>
+<button class="remove"></button>
 
-  $itemContainer.hide();
-
-  function inputLength(input) {
-    return $input.value.length();
+*/
+$submit.click(function( e ) {
+  e.preventDefault();
+  if ($input.value.length > 27) {
+    $input.value = "";
+    alert("Please enter an amount under 28 characters");
+    return false;
+  } else {
+    addListItem();
+    return false;
   }
+});
+  function addListItem() {
+    var $li = "<li class='listItem'>" +
+              "<p class='item-name'>"+$input.value+"</p>" +
+              "<button class='remove'></button>" +
+              "</li>";
+  $ul.append($li);
+  $input.value = "";
+  return false;
+}
 
-  function returnInputLength(input) {
-    return $input.value.length();
+$(document).on("click", ".listItem", function() {
+  if($done) {
+    $(this).find("button").removeClass("done");
+    $(this).removeClass("done");
+    $done = false;
+  } else {
+    $(this).find("button").addClass("done");
+    $(this).addClass("done");
+    $done = true;
   }
+});
 
-  function addItem(input){
-  }
+$(document).on("click", ".remove", function() {
+  $(this).parents("li").remove();
+});
+
 });
